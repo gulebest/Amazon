87469ac4d638211b64818cc898945a3bffbd3699
gulebest <gulumabest694@gmail.com>
2025-11-08 20:32:13 +0300

    product error

--- src/assets/Components/Product/Product.jsx
+++ src/assets/Components/Product/Product.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import ProductCard from './ProductCard';
+import classes from './ProductCard.module.css';
 import Loader from '../Loader/Loader';
-import classes from './Product.module.css';
 
 function Product() {
   const [products, setProducts] = useState([]);
